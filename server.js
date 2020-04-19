0833ecef1e5091a6453f85eb7ef53cb6294ee1bc
ahmcho <mail@ahmcho.com>
2020-04-19 20:51:58 +0400

    Added PostgresSQL

--- server.js
+++ server.js
@@ -2,39 +2,25 @@ const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
 
+const pg = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'abc456',
+      database : 'smart-brain'
+    }
+});
+
+// pg.select('*').from('users').then( data => {
+//     console.log(data);
+// })
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 app.use(cors());
-
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'Ahmad',
-            email: 'mail@ahmcho.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'John',
-            password: 'bananas',
-            email: 'carpenter@is.here.com',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'mail@ahmcho.com'
-        }
-    ]
-}
 
 app.get('/', (req,res) => {
     res.send(database.users)
@@ -42,51 +28,72 @@ app.get('/', (req,res) => {
 
 app.get('/profile/:id', (req,res) => {
     const {id} = req.params;
-    let found =false;
-    database.users.forEach(user => {
-        if(user.id === id){
-            found = true;
-            return res.json(user);
+    pg.select('*').from('users').where({id})
+    .then(user => {
+        if(user.length){
+            res.json(user[0]);
+        } else {
+            res.status(400).json('Not found');
         }
     })
-    if(!found){
-        res.status(400).json('no such user');
-    }
+    .catch( err => res.status(400).json('error getting user'));
 });
 
 app.post('/signin', (req,res) => {
-    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json('error logging in');
-    }
+    pg.select('email','hash').from('login')
+        .where('email' , '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if(isValid){
+               return pg.select('*').from('users').where('email', '=', req.body.email)
+                .then(user => {
+                    res.json(user[0]);
+                })
+                .catch(err =>  res.status(400).json('unable to get user'));
+            } else {
+                res.status(400).json('Wrong credentials');
+            }
+        })
+    .catch( err => res.status(400).json('Wrong credentials'));
 })
 
 app.post('/register', (req,res) => {
     const {email,name,password} = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date()
-    })
-    res.json(database.users[database.users.length - 1]);
+    const hash = bcrypt.hashSync(password);
+        pg.transaction( trx => {
+            trx.insert({
+                hash: hash,
+                email: email
+            })
+            .into('login')
+            .returning('email')
+            .then( loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0],
+                        name: name,
+                        joined: new Date()
+                    }).then(user => {
+                        res.json(user[0]);
+                    })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+        })
+        .catch(err => res.status(400).json('Unable to register'));
 })
 
 app.put('/image', (req,res) => {
     const {id} = req.body;
-    let found =false;
-    database.users.forEach(user => {
-        if(user.id === id){
-            found = true;
-            user.entries++
-            return res.json(user.entries);
-        }
+    pg('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
     })
-    if(!found){
-        res.status(400).json('no such user');
-    }
+    .catch(err => res.status(400).json('unable to get entries'))
 })
 
 app.listen(3000, () => {
