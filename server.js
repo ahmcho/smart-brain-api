8452976ebf8a30f6822beeb3c4589aa575f28dd8
ahmcho <mail@ahmcho.com>
2020-04-20 16:36:47 +0400

    added remote PostgresSQL

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const pg = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-silhouetted-65514',
       user : 'postgres',
       password : 'abc456',
       database : 'smart-brain'
